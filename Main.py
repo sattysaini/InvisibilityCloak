9a3d99b8f1493f1b4a0873117af9d4f3119baec9
sattysaini <sattysaini@users.noreply.github.com>
2020-07-14 22:52:26 +0530

    Editing the comments and names

--- Main.py
+++ Main.py
@@ -4,19 +4,18 @@ import time
 
 
 # taking video.mp4 as input.
-# Make your path according to your needs
+# Making the output file ready to tak output
 capture_video = cv2.VideoCapture("video.mp4")
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 out = cv2.VideoWriter('output.mp4', fourcc, 29.61, (640, 352))
+
 # give the camera to warm up
 time.sleep(1)
 count = 0
 background = 0
 
 # capturing the background in range of 60
-# you should have video that have some seconds
-# dedicated to background frame so that it
-# could easily save the background image
+# to save the background image
 for i in range(60):
     return_val, background = capture_video.read()
     if return_val == False:
@@ -33,14 +32,8 @@ while (capture_video.isOpened()):
     img = np.flip(img, axis=1)
 
     # convert the image - BGR to HSV
-    # as we focused on detection of red color
-
-    # converting BGR to HSV for better
-    # detection or you can convert it to gray
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # -------------------------------------BLOCK----------------------------#
-    # ranges should be carefully chosen
     # setting the lower and upper range for mask1
     lower_red = np.array([110, 60, 60])
     upper_red = np.array([110, 255, 255])
@@ -49,24 +42,22 @@ while (capture_video.isOpened()):
     lower_red = np.array([85, 60, 60])
     upper_red = np.array([100, 255, 255])
     mask2 = cv2.inRange(hsv, lower_red, upper_red)
-    # ----------------------------------------------------------------------#
 
-    # the above block of code could be replaced with
-    # some other code depending upon the color of your cloth
+    # combining the masks
     mask1 = mask1 + mask2
 
-    # Refining the mask corresponding to the detected red color
+    # Refining the mask corresponding to the detected color
     mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3),
                                                             np.uint8), iterations=2)
     mask1 = cv2.dilate(mask1, np.ones((3, 3), np.uint8), iterations=1)
     mask2 = cv2.bitwise_not(mask1)
 
-    # Generating the final output
+    # Generating the final output and saving it
     res1 = cv2.bitwise_and(background, background, mask=mask1)
     res2 = cv2.bitwise_and(img, img, mask=mask2)
     final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
     out.write(final_output)
-    cv2.imshow("INVISIBLE MAN", final_output)
+    cv2.imshow("INVISIBILITY CLOAK", final_output)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
